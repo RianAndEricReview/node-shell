01e3afa23e7d0d094bf608ff5d2f524c9032ad0b
Rian Halperin <rianhalperin@gmail.com>
2018-02-27 12:34:01 -0500

    Node Shell, implement echo command

--- bash.js
+++ bash.js
@@ -4,14 +4,15 @@ const commands = require('./commands.js')
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
+  let cmd = data.toString().split(' ')[0];
+  let args = data.toString().split(' ').slice(1).join(' ').trim()
 
   if(commands[cmd]){
-    commands[cmd]()
+    commands[cmd](args)
   } else {
     process.stdout.write('You typed: ' + cmd);
   }
-  process.stdout.write('\nprompt > ');
+
 
 });
 
--- commands.js
+++ commands.js
@@ -1,9 +1,32 @@
+const fs = require('fs')
+
 module.exports = {
   pwd: function(){
     process.stdout.write(process.cwd())
+    process.stdout.write('\nprompt > ');
   },
   date: function(){
     let today = new Date().toUTCString()
     process.stdout.write(today)
+    process.stdout.write('\nprompt > ');
+  },
+  ls: function(){
+    fs.readdir('.', (err, files) => {
+      if (err) throw err
+      files.forEach(file => {
+        process.stdout.write(file.toString() + '\n')
+      })
+      process.stdout.write('\nprompt > ');
+    })
+  },
+  echo: function(args){
+    args.split(' ').forEach(arg => {
+      if(arg[0] === '$'){
+        process.stdout.write(process.env[arg.slice(1).toUpperCase()] + ' ')
+      } else {
+        process.stdout.write(arg + ' ')
+      }
+    })
+    process.stdout.write('\nprompt > ')
   }
 }
